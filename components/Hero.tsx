9bd78ba604827dc48fcbdde33bbdf13afd09cdab
Sifan23 <sifanyg123@gmail.com>
2025-03-07 21:45:26 +0300

    changes on hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,9 @@
-import { FaLocationArrow } from "react-icons/fa6";
+import { FaLocationArrow, FaCopy } from "react-icons/fa6";
 
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import AuditedButton from "./AuditedButton";
 
 const Hero = () => {
   return (
@@ -11,17 +12,6 @@ const Hero = () => {
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
        */}
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-      </div>
 
       {/**
        *  UI: grid
@@ -40,33 +30,75 @@ const Hero = () => {
         />
       </div>
 
-      <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
-          </p>
-
+      <div className="flex justify-start relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-start justify-center">
           {/**
            *  Link: https://ui.aceternity.com/components/text-generate-effect
            *
            *  change md:text-6xl, add more responsive code
            */}
-          <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+          {/* <TextGenerateEffect
+            words="$KLINK The Ai"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-          />
-
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+          /> */}
+          <p className="text-center md:tracking-wider  text-[40px] md:text-[60px] lg:text-[76px] font-semibold tracking-[-1px] bg-purpleGradient bg-clip-text text-transparent">
+            $KLINK
           </p>
 
-          <a href="#about">
-            <MagicButton
-              title="Show my work"
-              icon={<FaLocationArrow />}
+          <p className="text-left md:tracking-wider text-[40px] md:text-[60px] lg:text-[76px] font-semibold tracking-[-1px]">
+            The AI-powered infrastructure for Web3 earnings
+          </p>
+          <div className="flex flex-row space-x-4 md:space-x-6">
+            <a href="#about">
+              <MagicButton
+                title="Buy $KLINK"
+                icon={<FaLocationArrow />}
+                position="right"
+                bgColor="linear-gradient(to right, #674EFF, #714EBD)"
+                borderColor="#9A8AFE"
+                textColor="#FFFFFF"
+                padding="16px 24px"
+                borderRadius="36.82px"
+                fontSize="16px"
+                fontWeight="600"
+              />
+            </a>
+            <a href="#about">
+              <MagicButton
+                title="Stake $KLINK"
+                icon={<FaLocationArrow />}
+                position="right"
+                bgColor="rgba(103, 78, 255, 0.1)" /* #674EFF1A */
+                borderColor="rgba(154, 138, 254, 0.5)" /* #9A8AFE80 */
+                textColor="#FFFFFF"
+                padding="16px 24px"
+                borderRadius="36.82px"
+                fontSize="16px"
+                fontWeight="600"
+              />
+            </a>
+          </div>
+          <div>
+            <p className="text-left mt-10 md:tracking-wider text-[14px] md:text-[15px] lg:text-[16px] font-semibold tracking-[-1px]">
+              Audited By
+            </p>
+            <AuditedButton />
+            {/* <MagicButton
+              title="Audited By"
+              imageSrc="/path-to-your-logo.png" // Auditor logo
+              address="0xSDAF...k67a"
               position="right"
-            />
-          </a>
+              divider={true} // Show the divider
+              icon={<FaCopy />} // Add the copy icon
+              bgColor="#55468B4D"
+              borderColor="#9C78ED33"
+              textColor="#FFFFFF"
+              padding="10px 16px"
+              borderRadius="16px"
+              fontSize="14px"
+              fontWeight="600"
+            /> */}
+          </div>
         </div>
       </div>
     </div>
