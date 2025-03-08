9dc772115c5688835f42bf2989853c7bdda54e11
Sifan23 <sifanyg123@gmail.com>
2025-03-08 17:33:54 +0300

    changes on the hero section layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import AuditedButton from "./AuditedButton";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="p-20 bg-black-100 rounded-[35px] rounded-tr-none">
       {/**
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
@@ -18,19 +18,8 @@ const Hero = () => {
        *  change bg color to bg-black-100 and reduce grid color from
        *  0.2 to 0.03
        */}
-      <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-       absolute top-0 left-0 flex items-center justify-center"
-      >
-        {/* Radial gradient for the container to give a faded look */}
-        <div
-          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-        />
-      </div>
 
-      <div className="flex justify-start relative my-20 z-10">
+      <div className="flex justify-start relative  z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-start justify-center">
           {/**
            *  Link: https://ui.aceternity.com/components/text-generate-effect
@@ -41,11 +30,11 @@ const Hero = () => {
             words="$KLINK The Ai"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           /> */}
-          <p className="text-center md:tracking-wider  text-[40px] md:text-[60px] lg:text-[76px] font-semibold tracking-[-1px] bg-purpleGradient bg-clip-text text-transparent">
+          <p className="text-center md:tracking-wider  text-[40px] m-0 p-0 md:text-[60px] lg:text-[76px] font-semibold tracking-[-1px] bg-purpleGradient bg-clip-text text-transparent">
             $KLINK
           </p>
 
-          <p className="text-left md:tracking-wider text-[40px] md:text-[60px] lg:text-[76px] font-semibold tracking-[-1px]">
+          <p className="text-left text-white m-0 p-0 md:tracking-wider text-[40px] md:text-[60px] lg:text-[76px] font-semibold tracking-[-1px]">
             The AI-powered infrastructure for Web3 earnings
           </p>
           <div className="flex flex-row space-x-4 md:space-x-6">
