9c7cae966b064f5c8149857bf5d0d6d8c16a9ae9
Sifan23 <sifanyg123@gmail.com>
2025-03-09 21:26:49 +0300

    wagmiclient provider added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import "./globals.css";
+import dynamic from "next/dynamic";
 import { ThemeProvider } from "./provider";
+
+const WagmiClientProvider = dynamic(() => import("@/app/WagmiClientProvider"), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,14 +26,16 @@ export default function RootLayout({
         <link rel="icon" href="/jsm-logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <WagmiClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </WagmiClientProvider>
       </body>
     </html>
   );
