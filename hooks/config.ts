81478b52628f18604d38b2375bf0261d3a31f412
Sifan23 <sifanyg123@gmail.com>
2025-03-09 21:24:16 +0300

    wagmi config

---
+++ hooks/config.ts
@@ -0,0 +1,14 @@
+import { http, createConfig } from "wagmi";
+import { base, mainnet, optimism } from "wagmi/chains";
+import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
+
+const projectId = "8837be5955b584f5d279d862b729a962";
+
+export const config = createConfig({
+  chains: [mainnet, base],
+  connectors: [injected(), walletConnect({ projectId }), metaMask(), safe()],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+  },
+});
