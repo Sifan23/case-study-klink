02acbb9d7e398944ae4abd70adfcd3ee77d3ced8
Sifan23 <sifanyg123@gmail.com>
2025-03-12 19:57:14 +0300

    connectwallet responsiveness update

--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -10,13 +10,12 @@ const ConnectWalletButton: React.FC<ConnectWalletButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className="mt-5 px-[20px] py-[16px] text-white rounded-[36.82px] flex items-center justify-center gap-[9.82px] text-center"
+      className="mt-5 px-[20px] py-[16px] text-white rounded-[36.82px] flex items-center justify-center gap-[9.82px] px-2 py-2 text-center text-[10px] sm:text-[16px] sm:px-[24px] sm:py-[18px] text-sm w-full sm:w-auto"
       style={{
         background: "linear-gradient(90deg, #674EFF 0%, #714EBD 100%)",
         border: "1.5px solid #9A8AFE",
-        fontFamily: "Inter",
         fontWeight: 600,
-        fontSize: "16px",
+        // fontSize: "16px", // Default font size for desktop
         lineHeight: "130%",
         letterSpacing: "-1%",
       }}
