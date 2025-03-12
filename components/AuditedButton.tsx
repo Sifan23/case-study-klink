2bbf30537a74daafd74b3c3fa7a6ef3d01f5026f
Sifan23 <sifanyg123@gmail.com>
2025-03-12 10:37:36 +0300

    updates on aduitedby button

--- components/AuditedButton.tsx
+++ components/AuditedButton.tsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { FaCopy } from "react-icons/fa";
+import { IoCopyOutline } from "react-icons/io5";
 
 const AuditedButton = () => {
   return (
-    <div className="flex flex-row justify-between items-center w-[290px] h-[44px] p-[10px 16px] border-2 border-[#9C78ED33] rounded-[16px] bg-[#55468B4D]">
-      {/* First part: Auditor's logo */}
-      <div className="flex items-center">
+    <div className="flex">
+      {/* Left Card */}
+      <div className="w-[159px] h-[53.31px] bg-[#29243f] flex items-center justify-center rounded-tl-[16px] rounded-bl-[16px] border-r-[1.5px] border-[#9C78ED33]">
         <img
-          src="/certik.svg" // Replace with the actual logo URL
+          src="/certik.svg"
           alt="Auditor Logo"
-          className="w-[50px] h-[50px] mr-3"
+          className="w-[97.43px] h-[24px]" // Updated size
         />
       </div>
 
-      {/* Divider */}
-      <div className="h-[24px] w-[1px] bg-gray-300 mx-4" />
-
-      {/* Second part: Address and copy icon */}
-      <div className="flex items-center flex-grow w-full">
-        <p className="text-[14px] font-medium text-gray-600">0xSDAF...k67a</p>
-        <FaCopy className="text-gray-600 cursor-pointer" />
+      {/* Right Card */}
+      <div className="w-[159px] h-[53.31px] bg-[#463964] flex items-center justify-between px-4 rounded-tr-[16px] rounded-br-[16px] border-l-[1.5px] border-[#9C78ED33]">
+        <p className="text-[14px] font-medium text-[#7767c5]">0xSDAF...k67a</p>
+        <IoCopyOutline className="text-[#7767c5] cursor-pointer" />
       </div>
     </div>
   );
