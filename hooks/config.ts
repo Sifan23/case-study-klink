b6f9a983d37b1efc95829dfc61ac719d6cead33d
Sifan23 <sifanyg123@gmail.com>
2025-03-12 14:34:11 +0300

    update on config

--- hooks/config.ts
+++ hooks/config.ts
@@ -2,7 +2,7 @@ import { http, createConfig } from "wagmi";
 import { base, mainnet, optimism } from "wagmi/chains";
 import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
 
-const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID ?? "";
 
 export const config = createConfig({
   chains: [mainnet, base],
